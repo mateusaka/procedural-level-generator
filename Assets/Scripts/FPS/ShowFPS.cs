34de4214e4d56a0ddb0bccf1434993841e3cbe21
Mateus <correia.mateus@hotmail.com>
2022-08-10 00:30:42 -0300

    FPS Script

---
+++ Assets/Scripts/FPS/ShowFPS.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowFPS : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI text;
+
+    private int lastFrameIndex;
+    private float[] frameDeltaTimeArray;
+
+    private void Awake() {
+        frameDeltaTimeArray = new float[50];
+    }
+
+    private void Update() {
+        frameDeltaTimeArray[lastFrameIndex] = Time.unscaledDeltaTime;
+        lastFrameIndex = (lastFrameIndex + 1) % frameDeltaTimeArray.Length;
+
+        text.text = Mathf.RoundToInt(CalculateFPS()).ToString();
+    }
+
+    private float CalculateFPS() {
+        float total = 0f;
+        foreach(float deltaTime in frameDeltaTimeArray) {
+            total += deltaTime;
+        }
+
+        return frameDeltaTimeArray.Length / total;
+    }
+}
